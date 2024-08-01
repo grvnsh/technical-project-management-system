1bb57f4a200248382bfbbd8d0a83c454973f9c3d
Gurvansh Gill <gurvanshsgill@gmail.com>
2024-08-01 18:16:00 -0400

    Update developer.h

--- developer.h
+++ developer.h
@@ -18,3 +18,12 @@ typedef struct {
     Task tasks[MAX_TASKS];
     int task_count;
 } Dev;
+
+void assign_task(Dev* dev, Task task);
+void complete_task(Dev* dev, const char* task_name);
+void transfer_task(Dev* from_dev, Dev* to_dev, const char* task_name);
+void remind_task(Task* task);
+void mark_as_late(Task* task);
+void display_developer_tasks(const Dev* dev);
+
+#endif
