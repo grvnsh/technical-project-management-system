c1a17393dac588686c6103d32b19fba3f5ab5cf2
Gurvansh Gill <gurvanshsgill@gmail.com>
2024-08-02 10:56:50 -0400

    Update event.h

--- event.h
+++ event.h
@@ -6,11 +6,22 @@
 #define MAX_NAME_LENGTH 50
 #define MAX_DESCRIPTION_LENGTH 200
 
-typedef struct {
+typedef enum {
+    TODO,
+    IN_PROGRESS,
+    COMPLETED
+} EventStatus;
+
+struct Event {
     char name[MAX_NAME_LENGTH];
     char frequency[MAX_NAME_LENGTH];
     char sponsor[MAX_NAME_LENGTH];
-} Event;
+    time_t start_date;
+    time_t end_date;
+    int priority;
+    EventStatus status;
+    int progress;
+};
 
 Event *create_event();
 bool edit_event(Event *event, const char *new_name, const char *new_frequency, const char *new_sponsor);
